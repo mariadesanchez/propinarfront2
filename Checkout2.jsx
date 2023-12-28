5c0a5b73c81f806cb03a6c124875b94f3efc5f31
mariade_snchz <calder07@gmail.com>
2023-12-28 19:03:26 -0300

    Checkout2

--- Checkout2.jsx
+++ Checkout2.jsx
@@ -1,4 +1,3 @@
-
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 
@@ -12,32 +11,38 @@ import QRCodeGenerator from './QRCodeGenerator'
 import { useLocation } from "react-router-dom";
 import Rating from './Rating'
 
-
 // Componente
 const Checkout = () => {
   useEffect(() => {
     localStorage.setItem('comments', JSON.stringify([]));
   }, []);
-  initMercadoPago(import.meta.env.VITE_PUBLICKEY, {
+
+  initMercadoPago(import.meta.env.VITE_PUBLICKEY_1, {
     locale: "es-AR",
   });
 
-  const [preferenceId, setPreferenceId] = useState(null);
+  initMercadoPago(import.meta.env.VITE_PUBLICKEY_2, {
+    locale: "es-AR",
+  });
+
+  const [preferenceId1, setPreferenceId1] = useState(null);
+  const [preferenceId2, setPreferenceId2] = useState(null);
   const [unitPriceInput, setUnitPriceInput] = useState("");
   const [error, setError] = useState("");
-  //para Generar el QR que se imprime
-  const [urlApp, setUrlApp] = useState("https://propinarfront2.vercel.app/checkout");
+  const [urlApp, setUrlApp] = useState("https://propinare.vercel.app");
   const [user, setUser] = useState("");
+
   const updateUser = () => {
     const storedUser = localStorage.getItem('user');
     setUser(storedUser || 'Lucky Snchz');
   };
-  const createPreference = async () => {
+
+  const createPreference = async (account) => {
     const unitPrice = parseFloat(unitPriceInput);
 
     if (isNaN(unitPrice) || unitPrice <= 0) {
       setError("El valor de la propina debe ser mayor a 0.");
-      return;
+      return null;
     } else {
       setError("");
     }
@@ -49,32 +54,39 @@ const Checkout = () => {
     };
 
     try {
-      const url_preference = "https://propin-ar-back2.vercel.app/create_preference";
-   
+      const url_preference = `https://propinar-backe.vercel.app/create_preference_${account}`;
+
+      // Calcula el monto para la cuenta 2 (5% del unitPrice)
+      const amountForAccount2 = unitPrice * 0.05;
 
       let response = await axios.post(url_preference, {
         items: [product],
         shipment_cost: 0,
+        amountForAccount2: amountForAccount2, // Pasa el monto a la API
       });
 
       const { id } = response.data;
-   
       return id;
     } catch (error) {
       console.log(error);
+      return null;
     }
   };
+
   const handleBuy = async () => {
-    const id = await createPreference();
-    if (id) {
-      setPreferenceId(id);
-    
+    const id1 = await createPreference("1");
+    const id2 = await createPreference("2");
+
+    if (id1 && id2) {
+      setPreferenceId1(id1);
+      setPreferenceId2(id2);
     }
   };
 
   const handlePriceChange = (event) => {
     setUnitPriceInput(event.target.value);
   };
+
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const paramValue = queryParams.get("status"); // approved --- reject
@@ -86,13 +98,12 @@ const Checkout = () => {
       updateUser();
     }
   }, [paramValue]);
+
   return (
     <>  
     {paramValueApproved ? (
       <>
-        
         <div style={{ textAlign: 'center', fontFamily: 'Arial, sans-serif', fontSize: '16px' }}>
-   
           <Rating />
         </div>
       </>):
@@ -112,15 +123,19 @@ const Checkout = () => {
         error={error.length > 0}
         helperText={error}
       />
-      {preferenceId ? (<Wallet initialization={{ preferenceId, redirectMode: "self" }} />):
-
-      (<Button
-        onClick={handleBuy}
-        style={{ backgroundColor: "#009ee3", color: "#fff" }}
-      >
-        Mi Propina
-      </Button>)}
-   <QRCodeGenerator urlApp={urlApp}/>
+      {preferenceId1 && preferenceId2 ? (
+        <>
+          <Wallet initialization={{ preferenceId: preferenceId1, redirectMode: "self" }} />
+          <Wallet initialization={{ preferenceId: preferenceId2, redirectMode: "self" }} />
+        </>) : (
+        <Button
+          onClick={handleBuy}
+          style={{ backgroundColor: "#009ee3", color: "#fff" }}
+        >
+          Mi Propina
+        </Button>
+      )}
+      <QRCodeGenerator urlApp={urlApp}/>
     </div>
     }
     </>
@@ -128,9 +143,3 @@ const Checkout = () => {
 };
 
 export default Checkout;
-
-
- 
-  
-
-
